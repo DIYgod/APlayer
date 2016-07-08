a5cc501f3bcceb40285abad6b8a910c352100ef7
DIYgod <diy.d.god@gmail.com>
2016-07-08 16:15:22 +0800

    font -> inline svg

--- src/APlayer.js
+++ src/APlayer.js
@@ -8,6 +8,23 @@
          * @constructor
          */
         constructor(option) {
+            this.svg = {
+                'play': ['0 0 16 31', 'M15.552 15.168q0.448 0.32 0.448 0.832 0 0.448-0.448 0.768l-13.696 8.512q-0.768 0.512-1.312 0.192t-0.544-1.28v-16.448q0-0.96 0.544-1.28t1.312 0.192z'],
+                'pause': ['0 0 17 32', 'M14.080 4.8q2.88 0 2.88 2.048v18.24q0 2.112-2.88 2.112t-2.88-2.112v-18.24q0-2.048 2.88-2.048zM2.88 4.8q2.88 0 2.88 2.048v18.24q0 2.112-2.88 2.112t-2.88-2.112v-18.24q0-2.048 2.88-2.048z'],
+                'volume-up': ['0 0 28 32', 'M13.728 6.272v19.456q0 0.448-0.352 0.8t-0.8 0.32-0.8-0.32l-5.952-5.952h-4.672q-0.48 0-0.8-0.352t-0.352-0.8v-6.848q0-0.48 0.352-0.8t0.8-0.352h4.672l5.952-5.952q0.32-0.32 0.8-0.32t0.8 0.32 0.352 0.8zM20.576 16q0 1.344-0.768 2.528t-2.016 1.664q-0.16 0.096-0.448 0.096-0.448 0-0.8-0.32t-0.32-0.832q0-0.384 0.192-0.64t0.544-0.448 0.608-0.384 0.512-0.64 0.192-1.024-0.192-1.024-0.512-0.64-0.608-0.384-0.544-0.448-0.192-0.64q0-0.48 0.32-0.832t0.8-0.32q0.288 0 0.448 0.096 1.248 0.48 2.016 1.664t0.768 2.528zM25.152 16q0 2.72-1.536 5.056t-4 3.36q-0.256 0.096-0.448 0.096-0.48 0-0.832-0.352t-0.32-0.8q0-0.704 0.672-1.056 1.024-0.512 1.376-0.8 1.312-0.96 2.048-2.4t0.736-3.104-0.736-3.104-2.048-2.4q-0.352-0.288-1.376-0.8-0.672-0.352-0.672-1.056 0-0.448 0.32-0.8t0.8-0.352q0.224 0 0.48 0.096 2.496 1.056 4 3.36t1.536 5.056zM29.728 16q0 4.096-2.272 7.552t-6.048 5.056q-0.224 0.096-0.448 0.096-0.48 0-0.832-0.352t-0.32-0.8q0-0.64 0.704-1.056 0.128-0.064 0.384-0.192t0.416-0.192q0.8-0.448 1.44-0.896 2.208-1.632 3.456-4.064t1.216-5.152-1.216-5.152-3.456-4.064q-0.64-0.448-1.44-0.896-0.128-0.096-0.416-0.192t-0.384-0.192q-0.704-0.416-0.704-1.056 0-0.448 0.32-0.8t0.832-0.352q0.224 0 0.448 0.096 3.776 1.632 6.048 5.056t2.272 7.552z'],
+                'volume-down': ['0 0 28 32', 'M13.728 6.272v19.456q0 0.448-0.352 0.8t-0.8 0.32-0.8-0.32l-5.952-5.952h-4.672q-0.48 0-0.8-0.352t-0.352-0.8v-6.848q0-0.48 0.352-0.8t0.8-0.352h4.672l5.952-5.952q0.32-0.32 0.8-0.32t0.8 0.32 0.352 0.8zM20.576 16q0 1.344-0.768 2.528t-2.016 1.664q-0.16 0.096-0.448 0.096-0.448 0-0.8-0.32t-0.32-0.832q0-0.384 0.192-0.64t0.544-0.448 0.608-0.384 0.512-0.64 0.192-1.024-0.192-1.024-0.512-0.64-0.608-0.384-0.544-0.448-0.192-0.64q0-0.48 0.32-0.832t0.8-0.32q0.288 0 0.448 0.096 1.248 0.48 2.016 1.664t0.768 2.528z'],
+                'volume-off': ['0 0 28 32', 'M13.728 6.272v19.456q0 0.448-0.352 0.8t-0.8 0.32-0.8-0.32l-5.952-5.952h-4.672q-0.48 0-0.8-0.352t-0.352-0.8v-6.848q0-0.48 0.352-0.8t0.8-0.352h4.672l5.952-5.952q0.32-0.32 0.8-0.32t0.8 0.32 0.352 0.8z'],
+                'loop': ['0 0 29 32', 'M25.6 9.92q1.344 0 2.272 0.928t0.928 2.272v9.28q0 1.28-0.928 2.24t-2.272 0.96h-22.4q-1.28 0-2.24-0.96t-0.96-2.24v-9.28q0-1.344 0.96-2.272t2.24-0.928h8v-3.52l6.4 5.76-6.4 5.76v-3.52h-6.72v6.72h19.84v-6.72h-4.8v-4.48h6.080z'],
+                'menu': ['0 0 22 32', 'M20.8 14.4q0.704 0 1.152 0.48t0.448 1.12-0.48 1.12-1.12 0.48h-19.2q-0.64 0-1.12-0.48t-0.48-1.12 0.448-1.12 1.152-0.48h19.2zM1.6 11.2q-0.64 0-1.12-0.48t-0.48-1.12 0.448-1.12 1.152-0.48h19.2q0.704 0 1.152 0.48t0.448 1.12-0.48 1.12-1.12 0.48h-19.2zM20.8 20.8q0.704 0 1.152 0.48t0.448 1.12-0.48 1.12-1.12 0.48h-19.2q-0.64 0-1.12-0.48t-0.48-1.12 0.448-1.12 1.152-0.48h19.2z']
+            };
+            this.getSVG = (type) => {
+                return `
+                    <svg xmlns:xlink="http://www.w3.org/1999/xlink" height="100%" version="1.1" viewBox="${this.svg[type][0]}" width="100%">
+                        <use xlink:href="#aplayer-${type}"></use>
+                        <path class="aplayer-fill" d="${this.svg[type][1]}" id="aplayer-${type}"></path>
+                    </svg>
+                `;
+            };
 
             this.isMobile = navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i);
             // compatibility: some mobile browsers don't suppose autoplay
@@ -177,7 +194,9 @@
             let eleHTML = `
                 <div class="aplayer-pic" ${(this.music.pic ? (`style="background-image: url('${this.music.pic}');"`) : ``)}>
                     <div class="aplayer-button aplayer-play">
-                        <i class="demo-icon aplayer-icon-play"></i>
+                        <button class="aplayer-icon aplayer-icon-play">`
+                +           this.getSVG('play')
+                + `     </button>
                     </div>
                 </div>
                 <div class="aplayer-info">
@@ -198,16 +217,25 @@
                             </div>
                         </div>
                         <div class="aplayer-time">
-                             - <span class="aplayer-ptime">00:00</span> / <span class="aplayer-dtime">00:00</span>
+                            <span class="aplayer-time-inner">
+                                - <span class="aplayer-ptime">00:00</span> / <span class="aplayer-dtime">00:00</span>
+                            </span>
                             <div class="aplayer-volume-wrap">
-                                <i class="demo-icon aplayer-icon-volume-down"></i>
+                                <button class="aplayer-icon aplayer-icon-volume-down">`
+                +                   this.getSVG('volume-down')
+                + `             </button>
                                 <div class="aplayer-volume-bar-wrap">
                                     <div class="aplayer-volume-bar">
                                         <div class="aplayer-volume" style="height: 80%; background: ${this.option.theme};"></div>
                                     </div>
                                 </div>
                             </div>
-                            <i class="demo-icon aplayer-icon-loop${(this.loop ? `` : ` aplayer-noloop`)}"></i>${(this.multiple ? `<i class="demo-icon aplayer-icon-menu"></i>` : ``)}
+                            <button class="aplayer-icon aplayer-icon-loop${(this.loop ? `` : ` aplayer-noloop`)}">`
+                +               this.getSVG('loop')
+                + `         </button>
+                            ${(this.multiple ? `<button class="aplayer-icon aplayer-icon-menu">`
+                +               this.getSVG('menu')
+                + `         </button>` : ``)}
                         </div>
                     </div>
                 </div>`;
@@ -331,7 +359,7 @@
             // control volume
             this.volumeBar = this.element.getElementsByClassName('aplayer-volume')[0];
             const volumeBarWrap = this.element.getElementsByClassName('aplayer-volume-bar')[0];
-            this.volumeicon = this.element.getElementsByClassName('aplayer-time')[0].getElementsByTagName('i')[0];
+            this.volumeicon = this.element.getElementsByClassName('aplayer-time')[0].getElementsByTagName('button')[0];
             const barHeight = 35;
             this.element.getElementsByClassName('aplayer-volume-bar-wrap')[0].addEventListener('click', (event) => {
                 const e = event || window.event;
@@ -343,12 +371,21 @@
             this.volumeicon.addEventListener('click', () => {
                 if (this.audio.muted) {
                     this.audio.muted = false;
-                    this.volumeicon.className = this.audio.volume === 1 ? 'demo-icon aplayer-icon-volume-up' : 'demo-icon aplayer-icon-volume-down';
+                    this.volumeicon.className = this.audio.volume === 1 ? 'aplayer-icon aplayer-icon-volume-up' : 'aplayer-icon aplayer-icon-volume-down';
+                    if (this.audio.volume === 1) {
+                        this.volumeicon.className = 'aplayer-icon aplayer-icon-volume-up';
+                        this.volumeicon.innerHTML = this.getSVG('volume-up');
+                    }
+                    else {
+                        this.volumeicon.className = 'aplayer-icon aplayer-icon-volume-down';
+                        this.volumeicon.innerHTML = this.getSVG('volume-down');
+                    }
                     this.updateBar('volume', this.audio.volume, 'height');
                 }
                 else {
                     this.audio.muted = true;
-                    this.volumeicon.className = 'demo-icon aplayer-icon-volume-off';
+                    this.volumeicon.className = 'aplayer-icon aplayer-icon-volume-off';
+                    this.volumeicon.innerHTML = this.getSVG('volume-off');
                     this.updateBar('volume', 0, 'height');
                 }
             });
@@ -395,8 +432,9 @@
 
             // toggle menu control
             if (this.multiple) {
+                const list = this.element.getElementsByClassName('aplayer-list')[0];
+                list.style.height = list.offsetHeight + 'px';
                 this.element.getElementsByClassName('aplayer-icon-menu')[0].addEventListener('click', () => {
-                    const list = this.element.getElementsByClassName('aplayer-list')[0];
                     if (!list.classList.contains('aplayer-list-hide')) {
                         list.classList.add('aplayer-list-hide');
                     }
@@ -571,7 +609,10 @@
                 this.button.classList.add('aplayer-pause');
                 this.button.innerHTML = '';
                 setTimeout(() => {
-                    this.button.innerHTML = '<i class="demo-icon aplayer-icon-pause"></i>';
+                    this.button.innerHTML = `
+                                <button class="aplayer-icon aplayer-icon-pause">`
+                        +           this.getSVG('pause')
+                        + `     </button>`;
                 }, 100);
 
                 // pause other players (Thanks @Aprikyblue)
@@ -582,7 +623,6 @@
                         }
                     }
                 }
-                console.log(Object.prototype.toString.call(time));
                 this.audio.play();
                 if (this.playedTime) {
                     clearInterval(this.playedTime);
@@ -609,7 +649,10 @@
                 this.button.classList.add('aplayer-play');
                 this.button.innerHTML = '';
                 setTimeout(() => {
-                    this.button.innerHTML = '<i class="demo-icon aplayer-icon-play"></i>';
+                    this.button.innerHTML = `
+                                <button class="aplayer-icon aplayer-icon-play">`
+                        +           this.getSVG('play')
+                        + `     </button>`;
                 }, 100);
                 this.audio.pause();
                 clearInterval(this.playedTime);
@@ -627,10 +670,12 @@
                 this.audio.muted = false;
             }
             if (percentage === 1) {
-                this.volumeicon.className = 'demo-icon aplayer-icon-volume-up';
+                this.volumeicon.className = 'aplayer-icon aplayer-icon-volume-up';
+                this.volumeicon.innerHTML = this.getSVG('volume-up');
             }
             else {
-                this.volumeicon.className = 'demo-icon aplayer-icon-volume-down';
+                this.volumeicon.className = 'aplayer-icon aplayer-icon-volume-down';
+                this.volumeicon.innerHTML = this.getSVG('volume-down');
             }
         }
 
