b4db267d957aba253ffbb69d8734c67bc55d09e8
DIYgod <diy.d.god@gmail.com>
2016-12-21 16:25:55 +0800

    build docs with docute

---
+++ docs/config.js
@@ -0,0 +1,13 @@
+self.$config = {
+  title: 'APlayer docs',
+  repo: 'DIYgod/APlayer',
+  'edit-link': 'https://github.com/DIYgod/APlayer/blob/gh-pages/docs',
+  icons: [
+    {
+      label: '关注我的微博',
+      svgId: 'i-weibo',
+      svgClass: 'weibo-icon',
+      link: 'http://weibo.com/anotherhome'
+    }
+  ],
+}
