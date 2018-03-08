7b9074ffd03b65f02c14e6ab0431ab5d72768a71
DIYgod <diy.d.god@gmail.com>
2018-03-08 18:21:10 +0800

    support lyric in `[mm:ss]` format

--- src/js/lrc.js
+++ src/js/lrc.js
@@ -79,28 +79,36 @@ class Lrc {
      * @return {String} [[time, text], [time, text], [time, text], ...]
      */
     parse (lrc_s) {
-        const lyric = lrc_s.split('\n');
-        const lrc = [];
-        const lyricLen = lyric.length;
-        for (let i = 0; i < lyricLen; i++) {
-            // match lrc time
-            const lrcTimes = lyric[i].match(/\[(\d{2}):(\d{2})\.(\d{2,3})]/g);
-            // match lrc text
-            const lrcText = lyric[i].replace(/\[(\d{2}):(\d{2})\.(\d{2,3})]/g, '').replace(/^\s+|\s+$/g, '');
+        if (lrc_s) {
+            const lyric = lrc_s.split('\n');
+            const lrc = [];
+            const lyricLen = lyric.length;
+            for (let i = 0; i < lyricLen; i++) {
+                // match lrc time
+                const lrcTimes = lyric[i].match(/\[(\d{2}):(\d{2})(\.(\d{2,3}))?]/g);
+                // match lrc text
+                const lrcText = lyric[i].replace(/\[(\d{2}):(\d{2})(\.(\d{2,3}))?]/g, '').replace(/^\s+|\s+$/g, '');
 
-            if (lrcTimes) {
-                // handle multiple time tag
-                const timeLen = lrcTimes.length;
-                for (let j = 0; j < timeLen; j++) {
-                    const oneTime = /\[(\d{2}):(\d{2})\.(\d{2,3})]/.exec(lrcTimes[j]);
-                    const lrcTime = oneTime[1] * 60 + parseInt(oneTime[2]) + parseInt(oneTime[3]) / ((oneTime[3] + '').length === 2 ? 100 : 1000);
-                    lrc.push([lrcTime, lrcText]);
+                if (lrcTimes) {
+                    // handle multiple time tag
+                    const timeLen = lrcTimes.length;
+                    for (let j = 0; j < timeLen; j++) {
+                        const oneTime = /\[(\d{2}):(\d{2})(\.(\d{2,3}))?]/.exec(lrcTimes[j]);
+                        const min2sec = oneTime[1] * 60;
+                        const sec2sec = parseInt(oneTime[2]);
+                        const msec2sec = oneTime[4] ? parseInt(oneTime[4]) / ((oneTime[4] + '').length === 2 ? 100 : 1000) : 0;
+                        const lrcTime = min2sec + sec2sec + msec2sec;
+                        lrc.push([lrcTime, lrcText]);
+                    }
                 }
             }
+            // sort by time
+            lrc.sort((a, b) => a[0] - b[0]);
+            return lrc;
         }
-        // sort by time
-        lrc.sort((a, b) => a[0] - b[0]);
-        return lrc;
+        else {
+            return [];
+        }
     }
 }
 
