70f2e00e13b7f09377ce05898361d86075b1d6e0
DIYgod <diy.d.god@gmail.com>
2018-03-12 01:03:38 +0800

    compatibility with old version

--- src/js/options.js
+++ src/js/options.js
@@ -3,16 +3,18 @@ export default (options) => {
     // default options
     const defaultOption = {
         container: options.element || document.getElementsByClassName('aplayer')[0],
-        mini: false,
+        mini: options.narrow || false,
         autoplay: false,
         mutex: true,
-        lrc: 0,
+        lrc: options.showlrc || 0,
         preload: 'auto',
         theme: '#b7daff',
         loop: 'all',
         order: 'list',
         volume: 0.7,
         listFolded: false,
+        listMaxHeight: options.listmaxheight,
+        audio: options.music
     };
     for (const defaultKey in defaultOption) {
         if (defaultOption.hasOwnProperty(defaultKey) && !options.hasOwnProperty(defaultKey)) {
@@ -23,6 +25,13 @@ export default (options) => {
     if (Object.prototype.toString.call(options.audio) !== '[object Array]') {
         options.audio = [options.audio];
     }
+
+    options.audio.map((item) => {
+        item.name = item.name || item.title;
+        item.artist = item.artist || item.author;
+        item.cover = item.cover || item.pic;
+        return item;
+    });
 
     if (options.audio.length <= 1 && options.loop === 'one') {
         options.loop = 'all';
