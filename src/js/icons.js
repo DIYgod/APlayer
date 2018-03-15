74f90639caf62b45f9b6830b52aa2acd21f6c376
Lianming.Wang <liam.wang@cootek.cn>
2018-03-15 14:20:27 +0800

    remove console

--- src/js/icons.js
+++ src/js/icons.js
@@ -1,12 +1,10 @@
 const context = require.context('../assets/', false, /\.svg/);
 const keys = context.keys();
-
 const Icons = {};
 
 for (let i = 0; i < keys.length; i += 1) {
     const key = keys[i].replace(/-([a-z])/g, (_, i) => i.toUpperCase()).match(/[\w\d]+(?=\.)/)[0];
     Icons[key] = context(keys[i]);
 }
-console.log(Icons);
 
 export default Icons;
