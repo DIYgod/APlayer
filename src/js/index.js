4af7cab5e75336dac3248503fbe6a5be891cfd13
DIYgod <diy.d.god@gmail.com>
2018-03-20 01:32:28 +0800

    fix messy console badge in Chrome 65

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,6 @@ import '../css/index.scss';
 import APlayer from './player';
 
 /* global APLAYER_VERSION GIT_HASH */
-console.log(`${'\n'} %c APlayer v${APLAYER_VERSION} ${GIT_HASH} %c http://aplayer.js.org ${'\n'}${'\n'}`, 'color: #fadfa3; background: #030307; padding:5px 0;', 'background: #fadfa3; padding:5px 0;');
+console.log(`${'\n'} %c APlayer v${APLAYER_VERSION} ${GIT_HASH} %c http://aplayer.js.org ${'\n'}`, 'color: #fadfa3; background: #030307; padding:5px 0;', 'background: #fadfa3; padding:5px 0;');
 
 export default APlayer;
