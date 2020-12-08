e20ed9b152ce5aa4b235745c49e960f243fd8eb7
在同一时空相遇 <52841727+yuannancheng@users.noreply.github.com>
2020-12-08 18:29:11 +0800

    Update utils.js

--- src/js/utils.js
+++ src/js/utils.js
@@ -27,7 +27,9 @@ const utils = {
         get: (key) => {
             try {
                 localStorage.getItem(key)
-            } catch (e) {}
+            } catch (e) {
+                return '{}'
+            }
         }
     },
 
